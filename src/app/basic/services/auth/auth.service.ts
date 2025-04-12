034b257b3759c30ce9c236f0463ef1b48fcc01f7
Prince <sagarprince8875@gmail.com>
2025-04-12 21:22:55 +0530

    initial commit

---
+++ src/app/basic/services/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const BASIC_URL = 'http://localhost:8080/';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient,) { }
+
+  registerClient(sigupRequestDTO: any): Observable<any> {
+    return this.http.post(BASIC_URL + "client/sign-up", sigupRequestDTO);
+  }
+
+  reqisterCompany(sigupRequestDTO: any): Observable<any> {
+    return this.http.post(BASIC_URL + "company/sign-up", sigupRequestDTO);
+  }
+}
