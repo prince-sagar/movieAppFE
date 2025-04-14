410ea7ebddcf2626d852243970926929988928e6
Prince <sagarprince8875@gmail.com>
2025-04-14 10:56:00 +0530

    Storage service to save and get token and user

--- src/app/basic/components/login/login.component.ts
+++ src/app/basic/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+    validateForm!: FormGroup;
+
+    constructor(private fb: FormBuilder,
+       private authService: AuthService,
+       private notification: NzNotificationService,
+       private router: Router) { }
+
+      ngOnInit(){
+        this.validateForm = this.fb.group({
+          userName: [null, [Validators.required]],
+          password: [null, [Validators.required]],
+        });
+      }
+
+      submitForm(){
+        this.authService.login(this.validateForm.get(['userName'])!.value, this.validateForm.get(['password'])!.value)
+        .subscribe(res =>{
+          console.log(res);
+
+        }, error => {
+          this.notification.error(
+            'Error',
+            '${error.error}',
+            { nzDuration: 5000 }
+          )
+        });
+
+      }
 
 }
